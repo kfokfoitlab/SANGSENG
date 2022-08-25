e1b53c44f74dedc5221f9c624109f012e74d2e26
xoqlxoql <xoql2002@kfo.or.kr>
2022-08-25 11:17:06 +0900

    -- 22_08_25_xoql2002 -- Routes.php => ID/PW 찾기 진행상황변경 경로 추가 ForgotInfo.php,ForgotInfoModel.php,Index.js => ID/PW 찾기 진행상황변경 로직 추가

--- src/admin/app/Models/Member/ForgotInfoModel.php
+++ src/admin/app/Models/Member/ForgotInfoModel.php
@@ -115,141 +115,22 @@
 			
 		} // }}}
 		
-		public function Detail($uuid)
-		{ //{{{
-			$data = [];
-			
+		public function statusUpdate($data)
+		{
 			$query = "
-            select
-                *
-                ,ST_X(coordinate) as latitude
-                ,ST_Y(coordinate) as logitude
-            from
-                ".$this->table_name."
-            where
-                uuid = '".$uuid."'
-            limit
-                1
-        ";
-			$this->rodb->query($query);
-			$row = $this->rodb->next_row();
+			UPDATE
+				".$this->table_name."
+			SET
+				status = ".$data["status"]."
+			WHERE
+				idx = ".$data["idx"]."
+			LIMIT 1
+			";
 			
-			$data = $row;
-			
-			return $data;
-		} //}}}
-		
-		public function Update($files, $data)
-		{ //{{{
-			helper(["specialchars"]);
-			
-			// profile image upload
-			$file = $files["profile_img"];
-			if($file["error"] == 0){
-				// 기존 파일 있으면 업데이트
-				$query = "
-                select
-                    profile_img_uuid
-                from
-                    ".$this->table_name."
-                where
-                    uuid = '".$data["uuid"]."'
-                limit 1
-            ";
-				$origin_profile_img_uuid = $this->rodb->simple_query($query);
-				$new_profile_img_uuid = $this->uploadFiles($file, $origin_profile_img_uuid);
-				$profile_img_uuid = ",profile_img_uuid = '".$new_profile_img_uuid."'";
-			}
-			else {
-				$profile_img_uuid = ",profile_img_uuid = null";
-			}
-			
-			// welfare card upload
-			$file = $files["welfare_img"];
-			if($file["error"] == 0){
-				// 기존 파일 있으면 업데이트
-				$query = "
-                select
-                    welfare_card_uuid
-                from
-                    ".$this->table_name."
-                where
-                    uuid = '".$data["uuid"]."'
-                limit 1
-            ";
-				$origin_welfare_card_uuid = $this->rodb->simple_query($query);
-				$new_welfare_card_uuid = $this->uploadFiles($file, $origin_welfare_card_uuid);
-				$welfare_card_uuid = ",welfare_card_uuid = '".$new_welfare_card_uuid."'";
-			}
-			else {
-				$welfare_card_uuid = ",welfare_card_uuid = null";
-			}
-			
-			// coordinate
-			$coor_x = @(float)$data["coordinate_x"];
-			$coor_y = @(float)$data["coordinate_y"];
-			$coordinate = "POINT(".$coor_x.", ".$coor_y.")";
-			
-			
-			// calcurating impairment score
-			$impairment_score = 0;
-			foreach($data["impairment"]["assistive_device"] as $score){
-				$impairment_score += @(int)$score;
-			}
-			foreach($data["impairment"]["degree"] as $score){
-				$impairment_score += @(int)$score;
-			}
-			foreach($data["impairment"]["physical_ability"] as $score){
-				$impairment_score += @(int)$score;
-			}
-			
-			$detail = specialchars($data["impairment"]["detail"]);
-			$detail0 = str_replace("\n", "\\n", $detail);
-			$detail0 = str_replace("\r", "\\r", $detail0);
-			$detail0 = str_replace("\t", "\\t", $detail0);
-			
-			$remark = specialchars($data["impairment"]["remark"]);
-			$remark = str_replace("\n", "\\n", $remark);
-			$remark = str_replace("\r", "\\r", $remark);
-			$remark = str_replace("\t", "\\t", $remark);
-			
-			$data["impairment"]["detail"] = $detail;
-			$data["impairment"]["remark"] = $remark;
-			
-			$impairment = json_encode($data["impairment"], JSON_UNESCAPED_UNICODE);
-			
-			
-			$query = "
-            update
-                ".$this->table_name."
-            set
-                 name = '".$data["name"]."'
-                ,email = '".$data["email"]."'
-                ,phone = '".$data["phone"]."'
-                ,tel = '".$data["tel"]."'
-                ,fax = '".$data["fax"]."'
-                ,post_code = '".$data["post_code"]."'
-                ,address = '".$data["address"]."'
-                ,address_detail = '".$data["address_detail"]."'
-                ,coordinate = ".$coordinate."
-                ".$profile_img_uuid."
-                ".$welfare_card_uuid."
-                ,impairment = '".$impairment."'
-                ,impairment_score = ".$impairment_score."
-                ,sns_homepage = '".$data["sns_homepage"]."'
-                ,sns_blog = '".$data["sns_blog"]."'
-                ,sns_facebook = '".$data["sns_facebook"]."'
-                ,sns_twitter = '".$data["sns_twitter"]."'
-                ,sns_linkedin = '".$data["sns_linkedin"]."'
-                ,sns_youtube = '".$data["sns_youtube"]."'
-                ,update_date = '".date("Y-m-d H:i:s")."'
-            where
-                uuid = '".$data["uuid"]."'
-            limit 1
-        ";
 			$this->wrdb->update($query);
 			
-		} //}}}
+			return 1;
+		}
 	}
 
 
