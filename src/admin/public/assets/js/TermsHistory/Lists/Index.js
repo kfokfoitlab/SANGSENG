adbc614945b7be9b7175618bf6e8eed87b0d6c11
jylim <kfojylim@gmail.com>
2022-09-01 17:52:47 +0900

    -- 22_09_01_jylim -- 근로자관리 List, Detail, Update 페이지 생성 약관동의이력 List, Detail 페이지 생성

---
+++ src/admin/public/assets/js/TermsHistory/Lists/Index.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+
+    // datatable {{{
+    var dt_option = {
+        "searching": false,
+        "processing": true,
+        "serverSide": true,
+        "autoWidth": false,
+        //"dom": 'lt<"float-left"i>p',
+        //"dom": 'ltip',
+        "exportFilename": "KFO_장애인취업포털_채용목록",
+        "exportTitle": "전체 채용 목록",
+        "ajax": {
+             url: "/"+_CONTROLLER+"/getList"
+            ,type: "POST"
+            ,data: function(data){
+                data.columns[1].search.value = $("#search-title").val();
+                data.columns[2].search.value = $("#search-recommended").val();
+                data.columns[3].search.value = $("#search-progress").val();
+                data.columns[4].search.value = $("#search-profession").val();
+                data.columns[5].search.value = $("#search-employment-type").val();
+                data.columns[6].search.value = $("#search-career").val();
+                data.columns[10].search.value = $("#search-daterange").val();
+
+                /*
+                data.columns[2].search.value = $("#search-elements").val();
+                data.columns[4].search.value = $("#search-text").val();
+                data.columns[12].search.value = $("#search-lot1").val();
+                data.columns[13].search.value = $("#search-lot2").val();
+                data.columns[14].search.value = $("#search-lot3").val();
+                */
+            }
+        },
+        "order": [
+             [0, "desc"]
+        ],
+        "columns": [
+             {title: "idx", data: "idx", visible: false}
+            ,{title: "약관", data: "title", visible: true, className: "text-nowrap"}
+            ,{title: "적용상태", data: "title", visible: true, className: "text-nowrap"}
+            ,{title: "등록일", data: "title", visible: true, className: "text-nowrap"}
+            ,{title: "메모", data: "title", visible: true, className: "text-nowrap"}
+            ,{title: "상태", data: "status", orderable: false, visible: true, className: "text-center text-nowrap",
+                "render": function( data, type, row, meta ){
+                    switch(data){
+                        case "1": return "<span class='badge text-primary'>적용</span>"; break;
+                        case "3": return "<span class='badge text-dark'>비적용</span>"; break;
+                    }
+                }
+            }
+            ,{title: "상세보기", data: "uuid", visible: true, orderable: false, className: "text-center noExport",
+                "render": function( data, type, row, meta ){
+                    var html = "";
+                        html += "<a";
+                        html += "   class='btn btn-sm btn-outline-primary text-nowrap'";
+                        html += "   href='/"+_CONTROLLER+"/Detail/"+data+"' target='detail'>";
+                        html += "   상세보기";
+                        html += "</a>";
+                    
+                    return html;
+
+                }
+            }
+        ],
+        "initComplete": function(settings, json)
+        {
+            $("form[name='form-searchbar'] .search").keypress(function(event){
+                if(event.keyCode == 13){
+                    $("form[name='form-searchbar']").submit(); 
+                } 
+            });
+
+            $("form[name='form-searchbar']").submit(function(e){
+                e.preventDefault();
+                dt.ajax.reload();
+                return false;
+            });
+
+            $(".filter").change(function(){
+                dt.ajax.reload();
+            });
+        },
+        "drawCallback": function( settings ) {
+            // 추천 해제
+            $("button.action-enable-recommended").click(function(){
+                const uuid = $(this).attr("data-uuid");
+                $.disableRecommended(uuid);
+            });
+            // 추천 등록
+            $("button.action-disable-recommended").click(function(){
+                const uuid = $(this).attr("data-uuid");
+                $.enableRecommended(uuid);
+            });
+        }
+    };
+
+    dt = $.dataTables($("#datatables"), dt_option);
+
+    /*
+    $("#datatables tbody").on("dblclick", "tr", function()
+    { // {{{
+        $(this).find("a").get(0).click();
+    }); // }}}
+    */
+
+
+    // }}}
+
+});
+
+
+$.extend({
+    "enableRecommended": function(uuid)
+    { //{{{
+        $.ajax({
+             url : "/" + _CONTROLLER + "/RecommendSubmit/enable/" + uuid
+            ,dataType:"json"
+            ,type: "GET"
+            ,success: function(res){
+                dt.ajax.reload(false, null);
+            }
+            ,error: function(res){
+                console.log("error :", res);
+            }
+        });
+    } //}}}
+
+    ,"disableRecommended": function(uuid)
+    { //{{{
+        $.ajax({
+             url : "/" + _CONTROLLER + "/RecommendSubmit/disable/" + uuid
+            ,dataType:"json"
+            ,type: "GET"
+            ,success: function(res){
+                dt.ajax.reload(false, null);
+            }
+            ,error: function(res){
+                console.log("error :", res);
+            }
+        });
+
+    } //}}}
+});
+
