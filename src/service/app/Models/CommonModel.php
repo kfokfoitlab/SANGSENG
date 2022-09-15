e2e623e1b5c02525ee8e0b8b15ad5ab1735beaf4
jinseok <jinseok@kfo.or.kr>
2022-09-15 11:20:04 +0900

    ------------------ --2022_09_15 jinseok ------------------------- CommonModel.php : 업로드 테스트

--- src/service/app/Models/CommonModel.php
+++ src/service/app/Models/CommonModel.php
@@ -329,7 +329,7 @@ class CommonModel extends dbModel
 		$name = $files["$fileName_ori"]['name'];
 		$exploded_file = explode(".",$name);
 		$ext = array_pop($exploded_file);
-		$target_dir = "var/www/SANGSENG/src/uploads/";
+		$target_dir = "D:/Project/SANGSENG/src/uploads/";
       //  $target_dir2 = ROOTPATH."public/uploads/upload_files/";
 		$file_tmp_name = $files["$fileName_ori"]["tmp_name"];
 
