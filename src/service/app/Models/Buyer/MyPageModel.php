22d7ef001269b9c0e49810d48fa236d2d2e7eaa9
jinseok <jinseok@kfo.or.kr>
2022-09-22 18:44:50 +0900

    ------------------ --2022_09_22 jinseok ------------------------- Detail.html,Detail.html,Index.html,MyPageModel.php,Left.html 검수사항

--- src/service/app/Models/Buyer/MyPageModel.php
+++ src/service/app/Models/Buyer/MyPageModel.php
@@ -284,7 +284,7 @@ class MyPageModel extends CommonModel
         $where_query = "";
 
         if($_GET["search_A"] != ""){
-            $where_query = $where_query." and contract_no like '%".$_GET["search_A"]."%'";
+            $where_query = $where_query." and a.product_name like '%".$_GET["search_A"]."%'";
         }
         if($_GET["search_B"] != "all" && $_GET["search_B"] != ""){
             if($_GET["search_B"] == "1"){
