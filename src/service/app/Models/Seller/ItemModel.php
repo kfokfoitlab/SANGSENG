ebe47951480c6df4a16559919e8d079e6fc26365
jinseok <jinseok@kfo.or.kr>
2022-10-19 17:11:49 +0900

    ------------------ --2022_10_19 jinseok ------------------------- admin Routes.php :경로추가 Lists.php,ContractModel.php,Index.js : 계약 삭제
    ProductModel.php, Detail.html,
    Index.html, Update.html: 상품 이미지 수정
    
    service
    ItemModel.php,ItemUpdate.html
    
    Help.html :감면액 도우미 계약서 미선택 처리

--- src/service/app/Models/Seller/ItemModel.php
+++ src/service/app/Models/Seller/ItemModel.php
@@ -8,22 +8,27 @@ class ItemModel extends CommonModel
     public function Register($files, $data, $table_name = "seller_product"){
         $allowed_ext = array('jpg','jpeg','png','gif','pdf','PNG');
 
+
         if($files["representative_image"]["name"] != ""){
+            $representative_ori = $files["representative_image"]["name"];
             $upload_representative_ori = "representative_image";
             $upload_representative = uniqid().".".pathinfo($files["representative_image"]["name"], PATHINFO_EXTENSION);
             $this->uploadFileNew($files,$upload_representative,$allowed_ext,$upload_representative_ori);
         }
         if($files["product_image1"]["name"] != ""){
+            $product_image1_ori = $files["product_image1"]["name"];
             $upload_image1_ori = "product_image1";
             $upload_image1 = uniqid().".".pathinfo($files["product_image1"]["name"], PATHINFO_EXTENSION);
             $this->uploadFileNew($files,$upload_image1,$allowed_ext,$upload_image1_ori);
         }
         if($files["product_image2"]["name"] != ""){
+            $product_image2_ori = $files["product_image2"]["name"];
             $upload_image2_ori = "product_image2";
             $upload_image2 = uniqid().".".pathinfo($files["product_image2"]["name"], PATHINFO_EXTENSION);
             $this->uploadFileNew($files,$upload_image2,$allowed_ext,$upload_image2_ori);
         }
         if($files["detail_img"]["name"] != ""){
+            $detail_img_ori = $files["detail_img"]["name"];
             $upload_detail_image_ori = "detail_img";
             $upload_detail_image = uniqid().".".pathinfo($files["detail_img"]["name"], PATHINFO_EXTENSION);
             $this->uploadFileNew($files,$upload_detail_image,$allowed_ext,$upload_detail_image_ori);
@@ -86,6 +91,10 @@ class ItemModel extends CommonModel
               ,product_image1 = '".$upload_image1."'
               ,product_image2 = '".$upload_image2."'
               ,detail_img = '".$upload_detail_image."'
+              ,representative_image_ori = '".$representative_ori."'
+              ,product_image1_ori = '".$product_image1_ori."'
+              ,product_image2_ori = '".$product_image2_ori."'
+              ,detail_img_ori = '".$detail_img_ori."'
               ,reduction = '".$reduction."'
               ,register_date = '".date("Y-m-d H:i:s")."'
               ,register_id = '".$uuid."'
@@ -140,7 +149,7 @@ public function ItemUpdateSubmit($files, $data){
                     from
                         seller_company_worker
                     where
-                         uuid ='".$uuid."'
+                         register_id ='".$uuid."'
                          and disability_degree ='1'
                     limit 1
                 ";
@@ -148,38 +157,58 @@ public function ItemUpdateSubmit($files, $data){
     $severely_disabled_cnt = $this->rodb->next_row();
     $severely_disabled = $severely_disabled_cnt["severely_disabled_cnt"];
 
+    $seller_info =[];
+    $seller_query = "
+			SELECT * FROM seller_company
+			WHERE uuid='".$uuid."'
+		";
+    $this->rodb->query($seller_query);
+    while($row = $this->rodb->next_row()) {
+        $seller_info = $row;
+    }
 
-    $contribution = $data["product_price"]/$data["seller_sales"];
+    $contribution = $data["product_price"]/$seller_info["seller_sales"];
     $workers = $mild_disabled+($severely_disabled*2);
     $reduction = $contribution * $workers;
     $reduction = round($reduction,4);
 
     $allowed_ext = array('jpg','jpeg','png','gif','pdf','PNG');
-	$upload_representative = $data["representative_image_ori_name"];
-	$upload_image1 = $data["product_image1_ori_name"];
-	$upload_image2 = $data["product_image2_ori_name"];
-   
-    $upload_representative_ori = "representative_image";
-	if($files["representative_image"]["name"] != "") {
-		$upload_representative = uniqid() . "." . pathinfo($files["representative_image"]["name"], PATHINFO_EXTENSION);
-		$this->uploadFileNew($files, $upload_representative, $allowed_ext, $upload_representative_ori);
-	}
 
-    $upload_image1_ori = "product_image1";
-	if($files["product_image1"]["name"] != "") {
-		$upload_image1 = uniqid() . "." . pathinfo($files["product_image1"]["name"], PATHINFO_EXTENSION);
-		$this->uploadFileNew($files, $upload_image1, $allowed_ext, $upload_image1_ori);
-	}
-
-    $upload_image2_ori = "product_image2";
-	if($files["product_image2"]["name"] != "") {
-		$upload_image2 = uniqid() . "." . pathinfo($files["product_image2"]["name"], PATHINFO_EXTENSION);
-		$this->uploadFileNew($files, $upload_image2, $allowed_ext, $upload_image2_ori);
-	}
+    if($files["representative_image"]["name"] != ""){
+        $representative_ori = $files["representative_image"]["name"];
+        $upload_representative_ori = "representative_image";
+        $upload_representative = uniqid().".".pathinfo($files["representative_image"]["name"], PATHINFO_EXTENSION);
+        $this->uploadFileNew($files,$upload_representative,$allowed_ext,$upload_representative_ori);
+    }else{
+        $representative_ori = $data["representative_image"];
+        $upload_representative = $data["representative_image"];
+    }
+    if($files["product_image1"]["name"] != ""){
+        $product_image1_ori = $files["product_image1"]["name"];
+        $upload_image1_ori = "product_image1";
+        $upload_image1 = uniqid().".".pathinfo($files["product_image1"]["name"], PATHINFO_EXTENSION);
+        $this->uploadFileNew($files,$upload_image1,$allowed_ext,$upload_image1_ori);
+    }else{
+        $product_image1_ori = $data["product_image1"];
+        $upload_image1 = $data["product_image1"];
+    }
+    if($files["product_image2"]["name"] != ""){
+        $product_image2_ori = $files["product_image2"]["name"];
+        $upload_image2_ori = "product_image2";
+        $upload_image2 = uniqid().".".pathinfo($files["product_image2"]["name"], PATHINFO_EXTENSION);
+        $this->uploadFileNew($files,$upload_image2,$allowed_ext,$upload_image2_ori);
+    }else{
+        $product_image2_ori = $data["product_image2"];
+        $upload_image2 = $data["product_image2"];
+    }
     if($files["detail_img"]["name"] != ""){
+        $detail_img_ori = $files["detail_img"]["name"];
         $upload_detail_image_ori = "detail_img";
         $upload_detail_image = uniqid().".".pathinfo($files["detail_img"]["name"], PATHINFO_EXTENSION);
         $this->uploadFileNew($files,$upload_detail_image,$allowed_ext,$upload_detail_image_ori);
+    }else{
+        $detail_img_ori = $data["detail_img"];
+        $upload_detail_image = $data["detail_img"];
     }
 
     $product_no = $data["product_no"];
@@ -201,7 +230,11 @@ public function ItemUpdateSubmit($files, $data){
                 ,representative_image = '".$upload_representative."'
                 ,product_image1 = '".$upload_image1."'
                 ,product_image2 = '".$upload_image2."'
-                ,detail_img = '".$upload_detail_image."'            
+                ,detail_img = '".$upload_detail_image."'
+                ,representative_image_ori = '".$representative_ori."'
+                ,product_image1_ori = '".$product_image1_ori."'
+                ,product_image2_ori = '".$product_image2_ori."'
+                ,detail_img_ori = '".$detail_img_ori."'
                 ,update_id  = '".$uuid."'      
                 ,reduction  = '".$reduction."'  
                 ,update_date = '".date("Y-m-d H:i:s")."'
