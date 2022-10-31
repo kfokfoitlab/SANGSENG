c84111e0899f151d75bc62d9cf82dc14cabbd32d
jinseok <jinseok@kfo.or.kr>
2022-10-31 17:33:18 +0900

    ------------------ --2022_10_31 jinseok ------------------------- MyPage.php : 계약상태 최신화

--- src/service/app/Controllers/Buyer/MyPage.php
+++ src/service/app/Controllers/Buyer/MyPage.php
@@ -44,21 +44,22 @@ class MyPage extends BaseController
         $result = $this->mypage_model->ContractStatus($_POST);
         if($result == 1 ){
             $_SESSION["Contract"]= $this->sigin_model->getContractList();
+            $_SESSION["ReductionMoney"]= $this->sigin_model->BuyerReduction();
             echo "
-        <script>
-        alert('최신화되었습니다');
-        location.href = '/Buyer/MyPage/Contract';
-</script>
-        ";
-        }else{
-            echo "
-        <script>
-        alert('최신상태 입니다.');
-        location.href = '/Buyer/MyPage/Contract';
-</script>
-        ";
-        }
-    } // }}}
+                    <script>
+                    alert('최신화되었습니다');
+                    location.href = '/Buyer/MyPage/Contract';
+                    </script>
+                ";
+                }else{
+                    echo "
+                    <script>
+                    alert('최신상태 입니다.');
+                    location.href = '/Buyer/MyPage/Contract';
+                    </script>
+                ";
+            }
+        } // }}}
 
     public function Info()
     { // {{{
