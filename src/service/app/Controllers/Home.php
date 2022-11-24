7130241f0d56d5b56442edd60597d7f1724d8bc5
jinseok <jinseok@kfo.or.kr>
2022-11-24 15:34:42 +0900

    ------------------ --2022_11_24 jinseok ------------------------- Home.php, Buyer.php, NoticeBanner.html :메인페이지 공지사항

--- src/service/app/Controllers/Home.php
+++ src/service/app/Controllers/Home.php
@@ -2,20 +2,22 @@
 
 namespace App\Controllers;
 use App\Models\Management\Company\ApplicationModel;
-use App\Models\CompanyModel;
 use App\Models\DatabaseModel;
 use App\Models\Buyer\BuyerModel;
-
+use App\Models\Seller\SellerModel;
 class Home extends BaseController
 {
-    private $model;
     private $database_model;
     private $buyer_model;
+    private $seller_model;
+
     public function __construct()
     { //{{{
         $this->buyer_model = new BuyerModel;
         $this->application_model = new ApplicationModel;
         $this->database_model = new DatabaseModel;
+        $this->seller_model = new SellerModel;
+
     } //}}}
 
     public function index()
@@ -24,10 +26,13 @@ class Home extends BaseController
         $ranking = $this->buyer_model->RecommendationList($value);
         $new_product = $this->buyer_model->NewProductList();
         $reduction =  $this->buyer_model->ReductionMoney();
+        $notice_list = $this->seller_model->getNoticeList();
         $data = array(
             "data" => $ranking["data"],
             "reduction" => $reduction,
-            "new_product" => $new_product
+            "new_product" => $new_product,
+            "notice_list" => $notice_list
+
         );
 
         echo view("Common/Header.html");
